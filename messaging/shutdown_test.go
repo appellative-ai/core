204b35c3a0a2e3cb269315164b612c4511a29604
advanced-go <mark.wilson.go@outlook.com>
2025-02-22 12:53:26 -0600

    Update shutdown_test.go

--- messaging/shutdown_test.go
+++ messaging/shutdown_test.go
@@ -1,36 +1,73 @@
 package messaging
 
+import "fmt"
+
 type testShutdown struct {
-	running    bool
 	agentId    string
 	shutdownFn func()
 }
 
-// Message - send message
-func (t *testShutdown) Message(msg *Message) {
+// Enqueue - append a shutdown function to a queue
+func (t *testShutdown) Enqueue(f func()) {
+	t.shutdownFn = AddShutdown(t.shutdownFn, f)
 }
 
-// Add - add a shutdown function
-func (t *testShutdown) Add(f func()) {
-	t.shutdownFn = AddShutdown(t.shutdownFn, f)
+// Push - push a shutdown function onto a stack
+func (t *testShutdown) Push(f func()) {
+	if t.shutdownFn == nil {
+		t.shutdownFn = func() {}
+	}
+	t.shutdownFn = AddShutdown(f, t.shutdownFn)
 }
 
 // Shutdown - shutdown the agent
 func (t *testShutdown) Shutdown() {
-	if !t.running {
-		return
-	}
-	t.running = false
 	if t.shutdownFn != nil {
 		t.shutdownFn()
 	}
-	t.Message(NewControlMessage(t.agentId, t.agentId, ShutdownEvent))
 }
 
-func ExampleShutdown() {
-	//if sd, ok1 := m.(OnShutdown); ok1 {
-	//	sd.Add(func() {
-	//		d.m.Delete(m.Uri())
-	//	})
-	//}
+func ExampleShutdown_Queue() {
+	t := &testShutdown{agentId: "agent/test"}
+
+	t.Enqueue(func() { fmt.Printf("test: Enqueue(1) -> func(%v)\n", 1) })
+	t.Enqueue(func() { fmt.Printf("test: Enqueue(2) -> func(%v)\n", 2) })
+	t.Enqueue(func() { fmt.Printf("test: Enqueue(3) -> func(%v)\n", 3) })
+	t.Enqueue(func() { fmt.Printf("test: Enqueue(4) -> func(%v)\n", 4) })
+	t.shutdownFn()
+
+	//Output:
+	//test: Enqueue(1) -> func(1)
+	//test: Enqueue(2) -> func(2)
+	//test: Enqueue(3) -> func(3)
+	//test: Enqueue(4) -> func(4)
+
 }
+
+func ExampleShutdown_Stack() {
+	t := &testShutdown{agentId: "agent/test"}
+
+	t.Push(func() { fmt.Printf("test: Push(1) -> func(%v)\n", 1) })
+	t.Push(func() { fmt.Printf("test: Push(2) -> func(%v)\n", 2) })
+	t.Push(func() { fmt.Printf("test: Push(3) -> func(%v)\n", 3) })
+	t.Push(func() { fmt.Printf("test: Push(4) -> func(%v)\n", 4) })
+	t.shutdownFn()
+
+	//Output:
+	//test: Push(4) -> func(4)
+	//test: Push(3) -> func(3)
+	//test: Push(2) -> func(2)
+	//test: Push(1) -> func(1)
+
+}
+
+/*
+if sd, ok1 := t.(OnShutdown); ok1 {
+	sd.S
+}
+	sd.Add(func() {
+		d.m.Delete(m.Uri())
+}
+
+
+*/
