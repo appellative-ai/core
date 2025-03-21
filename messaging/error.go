9458a91bda83df73bff04ecd179d2c76164229ee
advanced-go <mark.wilson.go@outlook.com>
2025-03-21 09:35:45 -0500

    Adding config error status

--- messaging/error.go
+++ messaging/error.go
@@ -14,3 +14,11 @@ func MessageContentTypeError(agentId string, msg *Message) error {
 	err := errors.New(fmt.Sprintf("error: message content:%v is invalid for agent:%v and event:%v", msg.ContentType(), agentId, msg.Event()))
 	return err
 }
+
+func ConfigEmptyStatusError(agent Agent) *Status {
+	return NewStatusError(StatusInvalidArgument, errors.New("config map is nil"), agent.Uri())
+}
+
+func ConfigContentStatusError(agent Agent, key string) *Status {
+	return NewStatusError(StatusInvalidArgument, errors.New(fmt.Sprintf("config map does not contain key: %v", key)), agent.Uri())
+}
