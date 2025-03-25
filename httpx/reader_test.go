22b7be21fa259cdf06554f3566e4121cb41f6cfa
advanced-go <mark.wilson.go@outlook.com>
2025-03-25 07:22:07 -0500

    Update reader_test.go

--- httpx/reader_test.go
+++ httpx/reader_test.go
@@ -1,8 +1,10 @@
 package httpx
 
 import (
+	"bytes"
 	"fmt"
 	"io"
+	"time"
 )
 
 func ExampleEmptyReader() {
@@ -13,5 +15,25 @@ func ExampleEmptyReader() {
 
 	//Output:
 	//test: emptyReader() -> [buf:] [err:<nil>]
+
+}
+
+func ExampleReadAll() {
+	s := "this is new content"
+	r1 := bytes.NewReader([]byte(s))
+	r2 := bytes.NewReader([]byte(s))
+
+	go func() {
+		buf, err := io.ReadAll(r1)
+		fmt.Printf("test: io.ReadAll(r1) -> [buf:%v] [err:%v]\n", string(buf), err)
+	}()
+
+	buf, err := io.ReadAll(r2)
+	fmt.Printf("test: io.ReadAll(r2) -> [buf:%v] [err:%v]\n", string(buf), err)
+
+	time.Sleep(time.Second * 2)
+	//Output:
+	//test: io.ReadAll(r2) -> [buf:this is new content] [err:<nil>]
+	//test: io.ReadAll(r1) -> [buf:this is new content] [err:<nil>]
 
 }
