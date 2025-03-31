369d51cadc264149ed0ca26ce4375993ada6c2f7
advanced-go <mark.wilson.go@outlook.com>
2025-03-31 05:53:21 -0500

    Update message_test.go

--- httpx/message_test.go
+++ httpx/message_test.go
@@ -10,13 +10,21 @@ func ExampleConfigMessage() {
 	ex := func(r *http.Request) (*http.Response, error) {
 		return NewResponse(http.StatusTeapot, nil, nil), nil
 	}
-	m := NewConfigExchangeMessage(ex)
-	ex = ConfigExchangeContent(m)
+	m := NewConfigExchangeMessage(func(r *http.Request) (*http.Response, error) {
+		return NewResponse(http.StatusTeapot, nil, nil), nil
+	})
 
+	ex = ConfigExchangeContent(m)
 	resp, err := ex(req)
 	fmt.Printf("test: ConfigExchangeContent() -> [status:%v] [err:%v]\n", resp.StatusCode, err)
 
+	m = NewConfigExchangeMessage(ex)
+	ex = ConfigExchangeContent(m)
+	resp, err = ex(req)
+	fmt.Printf("test: ConfigExchangeContent() -> [status:%v] [err:%v]\n", resp.StatusCode, err)
+
 	//Output:
+	//test: ConfigExchangeContent() -> [status:418] [err:<nil>]
 	//test: ConfigExchangeContent() -> [status:418] [err:<nil>]
 
 }
