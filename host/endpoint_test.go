09484ae88dccd1672ece76e92e25a6761f3719b6
advanced-go <mark.wilson.go@outlook.com>
2025-04-13 08:24:24 -0500

    Updating

--- host/endpoint_test.go
+++ host/endpoint_test.go
@@ -1,5 +1,38 @@
 package host
 
+import (
+	"fmt"
+	"github.com/behavioral-ai/core/httpx"
+	"github.com/behavioral-ai/core/rest"
+	"net/http"
+	"net/http/httptest"
+)
+
+func Exchange(w http.ResponseWriter, r *http.Request, handler rest.Exchange) {
+	httpx.AddRequestId(r)
+	if handler == nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	resp, _ := handler(r)
+	httpx.WriteResponse(w, resp.Header, resp.StatusCode, resp.Body, r.Header)
+}
+
+func ExampleHost() {
+	r := httptest.NewRecorder()
+	req, _ := http.NewRequest("", "http://localhost:8081/github/advanced-go/search:google?q=golang", nil)
+
+	Exchange(r, req, func(r *http.Request) (*http.Response, error) {
+		return &http.Response{StatusCode: http.StatusOK}, nil
+	})
+
+	fmt.Printf("test: Exchange() -> [resp:%v]\n", r.Result().StatusCode)
+
+	//Output:
+	//test: Exchange() -> [resp:200]
+
+}
+
 func ExampleNewEndpoint() {
 
 }
