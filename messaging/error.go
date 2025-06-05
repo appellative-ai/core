d32d0b1658693629114537a4354c8aa493ecf7dd
advanced-go <mark.wilson.go@outlook.com>
2025-06-05 12:19:47 -0500

    Update error.go

--- messaging/error.go
+++ messaging/error.go
@@ -5,15 +5,15 @@ import (
 	"fmt"
 )
 
-func ConfigEmptyMapError(location string) *Status {
-	return NewStatus(StatusInvalidArgument, errors.New("config map is nil")).WithLocation(location)
+func EmptyMapError(location string) *Status {
+	return NewStatus(StatusInvalidArgument, errors.New("map is nil")).WithLocation(location)
 }
 
-func ConfigMapContentError(location string, key string) *Status {
-	return NewStatus(StatusInvalidArgument, errors.New(fmt.Sprintf("config map does not contain key: %v", key))).WithLocation(location)
+func MapContentError(location string, key string) *Status {
+	return NewStatus(StatusInvalidArgument, errors.New(fmt.Sprintf("map does not contain key: %v", key))).WithLocation(location)
 }
 
-func ConfigEmptyReviewError(location string) *Status {
+func EmptyReviewError(location string) *Status {
 	return NewStatus(StatusInvalidArgument, errors.New("review is nil")).WithLocation(location)
 }
 
