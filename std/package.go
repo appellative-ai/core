eb3444610413570b8c160dd8bd2d59dc7d8cc02a
appellative-ai <mark.wilson.ai@outlook.com>
2025-08-18 09:48:58 -0500

    Removing logx and origin

--- std/package.go
+++ std/package.go
@@ -1,18 +1,8 @@
 package std
 
-var (
-	Origin *OriginT
-)
-
 const (
 	ContentTypeText     = "text/plain charset=utf-8"
 	ContentTypeBinary   = "application/octet-stream"
 	ContentTypeJson     = "application/json"
 	ContentTypeTextHtml = "text/html"
 )
-
-func SetOrigin(m map[string]string) *Status {
-	o, status := NewOrigin(m)
-	Origin = &o
-	return status
-}
